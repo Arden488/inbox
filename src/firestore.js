0adc9f75e6538faaaae7af45a83ebda273d7a89f
Anton Samoilov <absamoilov88@gmail.com>
2021-01-19 22:05:36 +0200

    Add firestore config

---
+++ src/firestore.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPCjKQ2dilMT745ES3jOP1h9GpDzc5vNM",
+    authDomain: "inbox-daf98.firebaseapp.com",
+    projectId: "inbox-daf98",
+    storageBucket: "inbox-daf98.appspot.com",
+    messagingSenderId: "306537746626",
+    appId: "1:306537746626:web:d3ec76ab5ec1423e85eb5b",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
