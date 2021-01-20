e90614ed9f11d0d02f951af243ff8845c42537d4
Anton Samoilov <absamoilov88@gmail.com>
2021-01-20 23:44:06 +0200

    Create entries component

---
+++ src/components/Entries/Entries.jsx
@@ -0,0 +1,12 @@
+function Entries() {
+    return <div>
+        <h1>All entries</h1>
+        <ul>
+            <li><a href="">First entry</a></li>
+            <li><a href="">Second entry</a></li>
+            <li><a href="">Third entry</a></li>
+        </ul>
+    </div>
+}
+
+export default Entries;
