385d1094a28097315b74f9eb378fcef1c69dbd8c
Anton Samoilov <absamoilov88@gmail.com>
2021-01-21 23:18:12 +0200

    Get entries and pass them donw

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import firestore from "./firestore";
+import { useEffect, useState } from "react";
+import { firestore } from "./firestore";
 
 import Entries from "./components/Entries/Entries";
 import Editor from "./components/Editor/Editor";
@@ -6,9 +7,27 @@ import Editor from "./components/Editor/Editor";
 import "./App.css";
 
 function App() {
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        async function fetchEntries() {
+            const snapshot = await firestore.collection("entries").get();
+
+            const entries = snapshot.docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                };
+            });
+
+            setEntries(entries);
+        }
+        fetchEntries();
+    });
+
     return (
         <div className="App">
-            <Entries />
+            <Entries entries={entries} />
             <Editor />
         </div>
     );
