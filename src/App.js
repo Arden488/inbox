2fa18a984ee98c52acc4c5589edeaa77aa2fe147
Anton Samoilov <absamoilov88@gmail.com>
2021-01-25 14:30:49 +0200

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import { useEffect, useState } from "react";
-import { firestore } from "./firestore";
-
-import Entries from "./components/Entries/Entries";
-import Editor from "./components/Editor/Editor";
-
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
 function App() {
-    const [entries, setEntries] = useState([]);
-
-    useEffect(() => {
-        async function fetchEntries() {
-            const snapshot = await firestore.collection("entries").get();
-
-            const entries = snapshot.docs.map((doc) => {
-                return {
-                    id: doc.id,
-                    ...doc.data(),
-                };
-            });
-
-            setEntries(entries);
-        }
-        fetchEntries();
-    });
-
-    return (
-        <div className="App">
-            <Entries entries={entries} />
-            <Editor />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
 }
 
 export default App;
