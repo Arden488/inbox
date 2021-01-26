025f542ad4ac6a07f277a035dc8784e2505df125
Anton Samoilov <absamoilov88@gmail.com>
2021-01-26 23:53:43 +0200

    Display entries' titles

--- src/components/Entries/Entries.jsx
+++ src/components/Entries/Entries.jsx
@@ -1,8 +1,15 @@
 function Entries({ entries }) {
+    console.log(entries)
     return <div>
         <h1>All entries</h1>
         <ul>
-            {entries.map(entry => <li key={ entry.id }><a href="">{ entry.content }</a></li>)}
+            {entries.map(entry =>
+                <li key={entry.id}>
+                    <a href="">
+                        {entry.title || "Untitled"}
+                    </a>
+                </li>
+            )}
         </ul>
     </div>
 }
