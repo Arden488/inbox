e455959b89b5f94ad69bf2d18b4100b47638d23b
Anton Samoilov <absamoilov88@gmail.com>
2021-01-27 22:33:24 +0200

    Refactor entry layout

--- src/components/Entries/Entries.jsx
+++ src/components/Entries/Entries.jsx
@@ -2,12 +2,13 @@ function Entries({ entries }) {
     return <div>
         <h2>All entries</h2>
         {entries.map(entry => {
-            const date = new Date(entry.datetime * 1000)
-            const hasDate = date && date != "Invalid Date"
+            let date = 'Unknown date'
+            const seconds = entry.datetime?.seconds
+            if (seconds) date = new Date(seconds * 1000)
 
             return <a href="javascript:void(0)" className="entries__item" key={entry.id}>
                     {entry.title || "Untitled"}
-                    <span className="entries__item-date">{hasDate && date.toString()}</span>
+                    <span className="entries__item-date">{date.toLocaleString()}</span>
                 </a>
         })}
     </div>
