424302934adb289dd9367c7adefe8e02cbe1f7f8
Anton Samoilov <absamoilov88@gmail.com>
2021-01-28 23:50:46 +0200

    Add active entry display

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,4 +1,5 @@
 import { firestore } from "../../firestore";
+import { useState, useEffect } from "react";
 
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
@@ -11,92 +12,95 @@ import SimpleImage from "@editorjs/simple-image";
 import Underline from "@editorjs/underline";
 import Paragraph from "editorjs-paragraph-with-alignment";
 
-import { collectIdsAndDocs } from "../../utilities";
-import { useState } from "react";
+let editor = null;
 
-const editor = new EditorJS({
-    holder: "editorjs",
-    data: {
-        blocks: [
-            {
-                type: "header",
-                data: {
-                    text: "Editor.js",
-                    level: 2,
-                },
-            },
-            {
-                type: "paragraph",
-                data: {
-                    text:
-                        "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.",
-                },
-            },
-        ],
-    },
-    tools: {
-        header: {
-            class: Header,
-            inlineToolbar: true,
-        },
-        paragraph: {
-            class: Paragraph,
-            inlineToolbar: true,
-        },
-        delimiter: Delimiter,
-        list: {
-            class: List,
-            inlineToolbar: true,
-        },
-        // image: {
-        //     class: ImageTool,
-        //     config: {
-        //         endpoints: {
-        //             byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
-        //             byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
-        //         },
-        //     },
-        // },
-        image: SimpleImage,
-        linkTool: {
-            class: LinkTool,
-            config: {
-                endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching
-            },
-        },
-        attaches: {
-            class: AttachesTool,
-            config: {
-                endpoint: "http://localhost:8008/uploadFile",
-            },
-        },
-        underline: Underline,
-    },
-});
-
-function Editor() {
+function Editor({ entry, onCreate }) {
     const [docTitle, setDocTitle] = useState("")
 
-    const saveData = () => {
+    console.log(onCreate)
+
+    if (editor === null) {
+        editor = new EditorJS({
+            holder: "editorjs",
+            placeholder: "Type something...",
+            tools: {
+                header: {
+                    class: Header,
+                    inlineToolbar: true,
+                },
+                paragraph: {
+                    class: Paragraph,
+                    inlineToolbar: true,
+                },
+                delimiter: Delimiter,
+                list: {
+                    class: List,
+                    inlineToolbar: true,
+                },
+                // image: {
+                //     class: ImageTool,
+                //     config: {
+                //         endpoints: {
+                //             byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+                //             byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+                //         },
+                //     },
+                // },
+                image: SimpleImage,
+                linkTool: {
+                    class: LinkTool,
+                    config: {
+                        endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching
+                    },
+                },
+                attaches: {
+                    class: AttachesTool,
+                    config: {
+                        endpoint: "http://localhost:8008/uploadFile",
+                    },
+                },
+                underline: Underline,
+            },
+        });
+    }
+
+    useEffect(() => {
+        editor.isReady.then(() => {
+            // console.log(entry)
+            if (entry !== null) {
+                editor.blocks.render({ blocks: entry.content })
+                setDocTitle(entry.title)
+            }
+        });
+    }, [entry])
+
+    const handleNewItem = () => {
+        editor.blocks.clear()
+        setDocTitle("")
+    }
+
+    
+    function saveData() {
         editor.isReady.then(() => {
             editor.save().then(async (savedData) => {
-                const docRef = await firestore.collection('entries').add({
+                console.log('editor.save()')
+
+                onCreate({
                     title: docTitle,
-                    content: savedData.blocks
+                    data: savedData
                 })
-                const doc = await docRef.get()
 
-                const newPost = collectIdsAndDocs(doc)
-
-                console.log(newPost)
+                editor.blocks.clear();
+                setDocTitle("");
             });
         });
     }
 
     return (
         <div>
+            <button onClick={handleNewItem}>Add new item</button>
             <input type="text" name="doctitle" value={docTitle} onChange={e => setDocTitle(e.target.value)} placeholder="Enter the document's title" />
-            <div className="editor-js-wrapper" id="editorjs"></div>
+            <div className="editor-js-wrapper" id="editorjs" />
             <button
                 className="ce-example__button"
                 id="saveButton"
